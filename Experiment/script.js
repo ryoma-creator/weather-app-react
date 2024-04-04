53548232dbde61ec9a13684b32aaab9c2de2f176
Ryoma/Cross <ryoma111326@gmail.com>
2024-04-04 13:07:07 +0800

    Revise Error Spelling line 86

--- Experiment/script.js
+++ Experiment/script.js
@@ -81,6 +81,28 @@ console.log(capitalizeC);
 //Practice
 const lastLetter = "ryomaaepigjapijg"
 console.log(lastLetter.charAt(-1));
- 
+ //miss//charAt() メソッドに負のインデックスを渡すと、
+ //空文字列 "" が返されます。負のインデックスは、文字列の末尾から逆順に文字を数えるために使われるものではありません。そのため、charAt(-1) は正しい結果を返しません。
+
+//Practice
+console.log(lastLetter.length);
+//lengthだと長さが出るはずなので、数字が出てきます。１６です。
+//なぜ、これに対して、charAtを使うと、文字列が出てくるのでしょう
+
+//length プロパティは文字列の長さを返しますが、charAt() メソッドは文字列内の指定された位置にある文字を返します。
+//文字列のインデックスは0から始まります。
 
 
+//Assignment
+const Letter = "OdinProject";
+const lastLetter = Letter.charAt(Letter.length-1);
+
+//revise
+//Assignment
+const Letter = "OdinProject";
+const lastLetter = Letter.charAt(Letter.length-1);
+console.log(lastLetter);
+
+const Letter = "ryoma";
+const lastLetter = Letter.charAt(Letter.length-1);
+console.log(lastLetter);
