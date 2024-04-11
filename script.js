667a1befce7158dd317d0b9302be2f95df4d92a5
Ryoma/Cross <ryoma111326@gmail.com>
2024-04-11 17:33:45 +0800

    Adding Random Function

---
+++ script.js
@@ -0,0 +1,22 @@
+//Listを作る。//listの中には、3択がある
+//[ぐー,ちょき,ぱー]
+//プレイヤーは、そこから選択する
+
+//argumentをプレイヤーは選択できる
+//parameterへプレイヤーが選択したものがいく
+
+//関数が値を得て、答えをreturnする。
+
+//結果が出力される。
+
+const janken = ['Rock',`Paper`, `Scissors`];
+
+//ランダムで出す方法をまず知ろう。
+const janken_random = Math.floor( Math.random()*3);//0,1,2をランダムで返す関数が入っている状況
+document.write(janken_random);
+
+
+const getComputerChoice = () =>{};
+
+
+getComputerChoice()
