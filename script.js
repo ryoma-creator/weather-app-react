5029410e9664a36b8183387da16f2308cf6eb12a
Ryoma/Cross <ryoma111326@gmail.com>
2024-04-11 18:23:23 +0800

    Making getComputerChoise Function for RPS Game

--- script.js
+++ script.js
@@ -9,16 +9,18 @@
 
 //結果が出力される。
 
-const janken = ['Rock',`Paper`, `Scissors`];
+
 
 //ランダムで出す方法をまず知ろう。
-const janken_random = Math.floor( Math.random()*3);//0,1,2をランダムで返す関数が入っている状況
-document.getElementById("result").innerHTML = janken_random;
+const getComputerChoice = () =>{
+    const janken = ['Rock!!',`Paper!!`, `Scissors!!`];
+    const janken_random = Math.floor(Math.random()*3);//0,1,2をランダムで返す関数が入っている状況
+    document.getElementById("result").innerHTML = `Enemy use.... ${janken[janken_random]}`;
+};
 //IDの””をjaken_randomで出た0-2の数字へ変更上書きさせる関数。
 
 
 
-const getComputerChoice = () =>{};
 
 
 getComputerChoice()
