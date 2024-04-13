c7075c2ce0a74dee5c1018be31a97592a2c19faf
Ryoma/Cross <ryoma111326@gmail.com>
2024-04-13 10:07:28 +0800

    convert fuction _name to camelCase name

--- script_console.js
+++ script_console.js
@@ -1,6 +1,6 @@
 const getComputerChoice = () => {
     const janken = ['Rock',`Paper`, `Scissors`];
-    const janken_random = Math.floor(Math.random()*janken.length);
-    return janken[janken_random]
+    const jankenRandom = Math.floor(Math.random()*janken.length);
+    return janken[jankenRandom];
 };
 
