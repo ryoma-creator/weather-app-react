ed889fedf77ce781f2cd3695d6919490fc376757
Ryoma/Cross <ryoma111326@gmail.com>
2024-04-29 15:56:31 +0800

    Fix syntax error

--- Progate/src/components/Lesson.js
+++ Progate/src/components/Lesson.js
@@ -6,8 +6,8 @@ render(){
     return(
       <div className='lesson-card'>
           <div className='lesson-item'>
-            <p this.props.name></p>
-            <img src=this.props.image/>
+            <p >{this.props.name}</p>
+            <img src={this.props.image}/>
           </div>
         </div>
         );
