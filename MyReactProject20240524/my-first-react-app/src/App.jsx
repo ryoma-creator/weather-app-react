b52a690b5befc5118aeb9cdcd506ed3180a91650
Ryoma/Cross <ryoma111326@gmail.com>
2024-06-08 12:17:52 +0800

    Update

--- MyReactProject20240524/my-first-react-app/src/App.jsx
+++ MyReactProject20240524/my-first-react-app/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Person from './Person.js';
+
 
 function App() {
 
   return (
     <>
       <p>Hello World!</p>
+      <Person />
     </>
   )
 }
--- MyReactProject20240524/my-first-react-app/src/main.jsx
+++ MyReactProject20240524/my-first-react-app/src/main.jsx
@@ -4,11 +4,13 @@ import Greeting from './Greeting.jsx'
 import Bio from './Example.jsx'
 import './index.css'
 import TodoList from './TestComponent.jsx'
+import Person from './Person.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
     <Bio/>
     <TodoList />
+    <Person />
   </React.StrictMode>,
 );
