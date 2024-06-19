9452f001161a9c81a00520bfd9cdf2dbfbe1dcf5
Ryoma/Cross <ryoma111326@gmail.com>
2024-06-19 11:31:15 +0800

    correct label position

--- person-first_last-name-app/src/App.js
+++ person-first_last-name-app/src/App.js
@@ -10,24 +10,25 @@ function Person() {
   return (
     <>
       <h1>{fullName}</h1>
-
       <label>
+        First Name: 
         <input
           type="text"
           placeholder='Please Type your First Name'
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
         />
-
+      </label>
+      <br/>
+      <label>
+        Last Name:       
         <input
           type="text"
           placeholder='Please Type your Last Name'
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
         />
-
-      </label>
-      
+       </label>
     </>
   );
 }
