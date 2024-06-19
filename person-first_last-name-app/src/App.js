7a5f5ff2a1232fc526ae02a0ce6a67d87082a9b6
Ryoma/Cross <ryoma111326@gmail.com>
2024-06-19 11:14:54 +0800

    Update to create firstName and lastName by setState first time

--- person-first_last-name-app/src/App.js
+++ person-first_last-name-app/src/App.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
 
 function Person() {
-  const [person, setPerson] = useState({ name: 'John', age: 100 });
+  const [firstName, setfirtstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const fullName = firstName + lastName;
+
+
 
   return (
     <>
-      <h1>{person.name}</h1>
-      <h2>{person.age}</h2>
+      <h1>{fullName}</h1>
+
+      <label>
+        <input
+          className='firstName'
+          value={firstName}
+          onInput={(e) => firtstName{e.target.value}}
+        >
+          first name
+        </input>
+
+        <input
+          className='lastName'
+          value={lastName}
+        >
+          last name
+        </input>
+
+      </label>
+      
     </>
   );
 }
--- sharing-state-example/src/App.js
+++ sharing-state-example/src/App.js
@@ -46,6 +46,15 @@ export default function Accordion() {
       >
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
       </Panel>
+
+      <Panel
+        title="Etymology"
+        isActive={activeIndexes.includes(2)}
+        onShow={() => handleShow(2)}
+        onHide={() => handleHide(2)}
+      >
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+      </Panel>
     </>
   );
 }
