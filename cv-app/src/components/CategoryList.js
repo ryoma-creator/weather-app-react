5de3bca12c8ef3079408c716986e10a0d17d71be
Ryoma/Cross <ryoma111326@gmail.com>
2024-06-21 08:03:38 +0800

    Refactor React.Fragment to span in order to add class

---
+++ cv-app/src/components/CategoryList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import '../styles/CategoryList.css'
+
+const categorysData = [
+    {id: 1, title: 'Profile Section'},
+    {id: 2, title: 'Education Section'},
+    {id: 3, title: 'Skills Sector'},
+    {id: 4, title: 'Mini Project'},
+    {id: 5, title: 'Social'},
+];
+
+const CategoryList = () => {
+    return (
+        <ul className="category-list">
+            {categorysData.map((item, index) => (
+                <CategoryItem key={index} item={item} />
+            ))}
+        </ul>
+    );
+};
+
+const CategoryItem = ({ item }) => {
+    const { id, title } = item;
+    const words = title.split(' ');
+
+    return (
+        <li className="category-item">
+            <span className="category-number">{id}</span>
+            <span className="category-title">
+                {words.map((word, index) => (
+                    <span key={index} className="category-word">
+                        {word}
+                        {index !== words.length - 1 && <br />}
+                    </span>
+                ))}
+            </span>
+        </li>
+    );
+};
+
+export default CategoryList;
