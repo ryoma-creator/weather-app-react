156df5426918596a5fe9b658632b32d62d998cbc
Ryoma/Cross <ryoma111326@gmail.com>
2024-07-13 17:46:41 +0800

    Implement basic CRUD operations for tasks in TodoList app

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,10 +1,23 @@
 import React from "react";
 import TodoItem from './TodoItem';
 
-function TodoList() {
+function TodoList( tasks, onDeleteTask, onEditTask, onToggleCompletion ) {
     return (
         <div className="todo-list">
-            asdfa
+            {tasks.length === 0 ? (
+                <p>Please add new task.</p>
+            ) : (
+                tasks.map(task =>(
+                    <TodoItem
+                        key={task.id}
+                        task={task}
+                        onDelete={onDeleteTask}
+                        onEdit={onEditTask}
+                        onToggleCompletion={onToggleCompletion}
+                    />
+                ))
+            )
+            }
         </div>
     );
 }
