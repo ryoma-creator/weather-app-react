8d2da0594d70b7558cc845abd94c9976a761e5af
Ryoma/Cross <ryoma111326@gmail.com>
2024-07-13 14:19:40 +0800

    Initialized basic structure for Todo List app
    - Created App component to render the main Todo List application.
    - Implemented TodoList component to display the list of tasks.
    - Added TodoItem component for rendering individual tasks within TodoList.

---
+++ todo-app/src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoItem() {
+    return(
+        <div className="todo-item">
+
+        </div>
+    );
+}
+
+export default TodoItem;
---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItem from './TodoItem';
+
+function TodoList() {
+    return (
+        <div className="todo-list">
+            asdfa
+        </div>
+    );
+}
+
+export default TodoList;
