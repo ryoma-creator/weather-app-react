dccd5172f2fc05bc4a745610724e645cb79dd884
Ryoma/Cross <ryoma111326@gmail.com>
2024-07-22 11:11:57 +0800

    update

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,27 +1,62 @@
 import React, { useState } from "react";
 
-function TodoItem({ key, task, onDelete, onEdit,onToggleCompletion}){
+//tasks{ id: Date.now(), name: taskName, completed: false }
+//Data example
+
+function TodoItem({ task, onDelete, onEdit, onToggleCompletion}){
     const [isEditing, setIsEditing] = useState(false);
     const [editedName, setEditedName] = useState(task.name);
 
     const handleEdit = () => {
         onEdit(task.id, editedName);
         setIsEditing(false);
-    };
+    };    
+// onEdit ⇩
+// const editTask = (taskId, newName) => {
+//     setTasks(tasks.map(task =>
+//       task.id === taskId ? { ...task, name: newName } : task
+//       ));
+//   };
 
+
+
+// {tasks.length === 0 ? (
+//     <p>Please add new task.</p>
+// ) : (
+// 要素が０より大きければ、to do listがあるのが前提なので以下が表示される
+// ToDoList.jsを参照
     return(
         <div className="todo-item">
             <input
                 type="checkbox"
                 checked={task.completed}
                 onChange={() => onToggleCompletion(task.id)}
+// const toggleTaskCompletion = (taskId) => {
+//     setTasks(tasks.map(task =>
+//       task.id === taskId ? { ...task, completed: !task.completed } : task
+//       ));
+//   };
+// チェックをon/offにするだけのもの
             />
+
+{/* const [isEditing, setIsEditing] = useState(false); */}
             {isEditing ? (
                     <input
                         type="text"
+// const [editedName, setEditedName] = useState(task.name);
                         value={editedName}
                         onChange={(e) => setEditedName(e.target.value)}
                         onBlur={handleEdit}
+// const handleEdit = () => {
+//     onEdit(task.id, editedName);
+//     setIsEditing(false);
+// }; 
+// onEdit ⇩
+// const editTask = (taskId, newName) => {
+//     setTasks(tasks.map(task =>
+//       task.id === taskId ? { ...task, name: newName } : task
+//       ));
+//   };   
                         autoFocus
                     />
             ) : (
@@ -33,6 +68,9 @@ function TodoItem({ key, task, onDelete, onEdit,onToggleCompletion}){
             </span>
             )}
             <button onClick={() => onDelete(task.id)}>Delete</button>
+            {/* const deleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  }; */}
         </div>
     );
 }
